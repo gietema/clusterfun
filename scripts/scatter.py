2301003b0b6a107f4959dfc38f82d8c0dc6d79f9
gietema <jochem@giete.ma>
2023-04-12 19:23:16 +0100

    Add script for scatter

---
+++ scripts/scatter.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pandas as pd
+import clusterfun as clt
+
+def main():
+    df = pd.DataFrame()
+    df["x"] = [1, 2, 3]
+    df["y"] = [1, 2, 3]
+    df["media"] = [file for file in (Path(__file__).parent.parent / "data" / "wikiart").rglob("*/*.jpg")][:3]
+    df["color"] = ["red", "blue", "green"]
+    clt.scatter(
+        df,
+        x="x",
+        y="y",
+        media="media",
+        color="color",
+        title="A title of a plot",
+    )
+
+
+if __name__ == '__main__':
+    main()
