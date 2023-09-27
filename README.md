1b7d113e591bbc6ba1ee3ec98ca9982b587be18e
gietema <jochem@giete.ma>
2023-09-27 12:03:00 +0100

    Georges instead of George in example

--- README.md
+++ README.md
@@ -185,7 +185,7 @@ import pandas as pd
 import clusterfun as clt
 
 df = pd.read_csv("https://raw.githubusercontent.com/gietema/clusterfun-data/main/wiki-art.csv")
-df = df[df.painter.isin(["Pablo Picasso", "Juan Gris", "George Braque", "Fernand Leger"])]
+df = df[df.painter.isin(["Pablo Picasso", "Juan Gris", "Georges Braque", "Fernand Leger"])]
 clt.violin(df, y="brightness", media="img_path")
 ```
 ![Example violin](data/violin.png)
--- scripts/violin.py
+++ scripts/violin.py
@@ -5,7 +5,7 @@ import clusterfun as clt
 
 def main():
     df = pd.read_csv("https://raw.githubusercontent.com/gietema/clusterfun-data/main/wiki-art.csv")
-    df = df[df.painter.isin(["Pablo Picasso", "Juan Gris", "George Braque", "Fernand Leger"])]
+    df = df[df.painter.isin(["Pablo Picasso", "Juan Gris", "Georges Braque", "Fernand Leger"])]
     print(clt.violin(df, y="brightness", media="img_path", show=False, color="painter"))
 
 
